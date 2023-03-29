fd565b92ba9effe3c76a76322d328834e4811ca3
uhassan7612420 <127688854+uhassan7612420@users.noreply.github.com>
2023-03-29 06:23:05 +0500

    added download url

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -18,5 +18,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name= 'index')
+    path('', views.index, name= 'index'),
+    path('downloadFile', views.downloadFile, name= 'downloadFile') 
 ]
